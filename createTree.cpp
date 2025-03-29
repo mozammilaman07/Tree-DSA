395c02372be7e748c404c831a23e681cc6017de4
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-03-29 23:01:43 +0530

    created class of Node

---
+++ createTree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->data = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
